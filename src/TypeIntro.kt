c7ae24c9bb01682c1e43905202ef308a1748ccdd
Robert Dunn <robertdunn220@gmail.com>
2019-08-23 12:27:45 -0400

    Type initialization

---
+++ src/TypeIntro.kt
@@ -0,0 +1,12 @@
+const val MAX_EXP = 5000
+
+fun main(args: Array<String>) {
+    val playerName = "Estragon"
+    var hasSteed = false
+    val pubName = "Unicorn's Horn"
+    val publicanName = "Fart Box"
+    val goldPieces = 50
+    val expPoints = 5
+
+    print(playerName.reversed())
+}
